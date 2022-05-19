a48f4746a1dcc4fa768a2e7ce58aa7fd16143c0d
QuyenVu04092000 <vuquyen.04092000@gmail.com>
2022-05-19 17:40:22 +0700

    completed

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,19 +1,5 @@
 import 'package:equatable/equatable.dart';
 
-enum WeatherCondition {
-  thunderstorm,
-  drizzle,
-  rain,
-  snow,
-  atmosphere, // dust, ash, fog, sand etc.
-  mist,
-  fog,
-  lightCloud,
-  heavyCloud,
-  clear,
-  unknown
-}
-
 class Weather extends Equatable{
 
   final String formattedCondition;
@@ -61,26 +47,5 @@ class Weather extends Equatable{
       clouds: jsonObject['clouds']['all'] as int,
       lastUpdated: jsonObject['dt_txt'].toString()
     );
-  }
-
-  static WeatherCondition _mapStringToWeatherCondition(String inputString){
-    Map<String, WeatherCondition> map = {
-      'Thunderstorm': WeatherCondition.thunderstorm,
-      'Drizzle': WeatherCondition.drizzle,
-      'Rain' : WeatherCondition.rain,
-      'Snow' : WeatherCondition.snow,
-      'Clear' : WeatherCondition.clear,
-      'Clouds' : WeatherCondition.heavyCloud ,
-      'Mist' : WeatherCondition.mist,
-      'fog' : WeatherCondition.fog,
-      'Smoke' : WeatherCondition.atmosphere,
-      'Haze' : WeatherCondition.atmosphere,
-      'Dust' : WeatherCondition.atmosphere,
-      'Sand' : WeatherCondition.atmosphere,
-      'Ash' : WeatherCondition.atmosphere,
-      'Squall' : WeatherCondition.atmosphere,
-      'Tornado' : WeatherCondition.atmosphere
-    };
-    return map[inputString] ?? WeatherCondition.unknown;
   }
 }
